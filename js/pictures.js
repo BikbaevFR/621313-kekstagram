cd690b06bb52872d33b5f4ea04104fa757d93753
FyargatBikbaev <bikbaevf18@gmail.com>
2018-11-22 19:14:33 +0400

    Нашел функцию для перемешивания массива с номерами Url картинок

--- js/pictures.js
+++ js/pictures.js
@@ -1,1 +1,25 @@
 'use strict';
+
+var mixArrays = function (array) {
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+var numberUrlPicture = [];
+
+for (var i = 1; i <= 25; i++) {
+  numberUrlPicture.push(i);
+  mixArrays(numberUrlPicture);
+}
