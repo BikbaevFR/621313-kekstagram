9231e8529bdfe11d40992824079c0ad97d6a2ebd
FyargatBikbaev <bikbaevf18@gmail.com>
2018-11-22 23:01:09 +0400

    Частично сделал генерацию массива с рандомными данными

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,27 @@
 'use strict';
 
-var mixArrays = function (array) {
+var comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+var descriptions = [
+  'Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!'
+];
+
+var quantityPictures = 25;
+
+// Выдает массив с рандомным, неповторяющимся порядком чисел
+var getMixArrays = function (array) {
   var currentIndex = array.length;
   var temporaryValue;
   var randomIndex;
@@ -21,5 +42,39 @@ var numberUrlPicture = [];
 
 for (var i = 1; i <= 25; i++) {
   numberUrlPicture.push(i);
-  mixArrays(numberUrlPicture);
+  getMixArrays(numberUrlPicture);
 }
+
+// Выдает рандомное число
+var getRandomInteger = function (min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  rand = Math.round(rand);
+  return rand;
+};
+
+var randomNumberLikes = getRandomInteger(15, 200);
+
+// Выдает рандомный массив с параметрами картинок
+var getRandomObjectPicture = function (array1, array2) {
+  var randomObjectPicture = [];
+
+  for (var index = 0; index < quantityPictures; index++) {
+    var randomCommentIndex = getRandomInteger(0, array1.length - 1);
+    var randomDescriptionIndex = getRandomInteger(0, array2.length - 1);
+
+    var newPicture = {
+      url: numberUrlPicture[index].toString(),
+      likes: randomNumberLikes,
+      comments: comments[randomCommentIndex],
+      description: descriptions[randomDescriptionIndex]
+    };
+
+    randomObjectPicture.push(newPicture);
+  }
+
+  return randomObjectPicture;
+};
+
+var pictures = getRandomObjectPicture(comments, descriptions);
+// Сделал временно, потому что линтер ругается)
+getRandomObjectPicture(pictures);
