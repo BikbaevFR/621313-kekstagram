0fe4fa47cf42e7d2d2dba339178c4115ee83a1f5
FyargatBikbaev <bikbaevf18@gmail.com>
2019-01-05 00:33:49 +0400

    Сделал фильтры и загрузку комментариев

--- js/picture.js
+++ js/picture.js
@@ -4,6 +4,16 @@
   var сontainerPictures = document.querySelector('.pictures');
   var similarPictureTemplate = document.querySelector('#picture').content;
 
+  var filtersSection = document.querySelector('.img-filters');
+  var filterPopular = document.querySelector('#filter-popular');
+  var filterNew = document.querySelector('#filter-new');
+  var filterDiscussed = document.querySelector('#filter-discussed');
+  var filterActiveClassName = 'img-filters__button--active';
+  var currentFilter = filterPopular;
+
+  var picturesArray = [];
+  var picturesArrayCopy = [];
+
   // Отдает рандомную картинку созданную на основе шаблона
   var renderPicture = function (picture) {
     // Копируем шаблон
@@ -16,19 +26,22 @@
     return pictureElement;
   };
 
-  var renderPictures = function (arrayPictures, length) {
+  var renderPictures = function (arrayPictures) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < length; i++) {
+    for (var i = 0; i < arrayPictures.length; i++) {
       fragment.appendChild(renderPicture(arrayPictures[i]));
     }
     сontainerPictures.appendChild(fragment);
     var thumbnails = document.querySelectorAll('.picture__img');
-    window.showBigPicture(thumbnails, arrayPictures);
+    window.pickUpBigPicture(thumbnails, arrayPictures);
   };
 
   // Успешное выполнение запроса
   var succsessDownloadHandler = function (picturesData) {
-    renderPictures(picturesData, picturesData.length);
+    renderPictures(picturesData);
+    filtersSection.classList.remove('img-filters--inactive');
+    filterPopular.classList.add(filterActiveClassName);
+    picturesArray = picturesData;
   };
 
   // Неуспешное выполнение запроса
@@ -45,4 +58,80 @@
   };
 
   window.backend.downLoad(succsessDownloadHandler, errorDownloadHandler);
+
+  var showFilterActive = function (elem) {
+    elem.classList.add(filterActiveClassName);
+  };
+
+  var hideFilterActive = function (elem) {
+    elem.classList.remove(filterActiveClassName);
+  };
+
+  // Удаляем картинки
+  var deletePictures = function () {
+    var deletedPictures = сontainerPictures.querySelectorAll('.picture');
+    for (var i = 0; i < deletedPictures.length; i++) {
+      сontainerPictures.removeChild(deletedPictures[i]);
+    }
+  };
+
+  // Получаем рандомный массив (10 элементов)
+  var getRandomArray = function (array, n) {
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    var shortenedArray = array.slice(0, n);
+    return shortenedArray;
+  };
+
+  filterPopular.addEventListener('click', function () {
+    if (currentFilter) {
+      hideFilterActive(currentFilter);
+    }
+    showFilterActive(filterPopular);
+    currentFilter = filterPopular;
+    deletePictures();
+    renderPictures(picturesArray);
+  });
+
+  filterNew.addEventListener('click', function () {
+    if (currentFilter) {
+      hideFilterActive(currentFilter);
+    }
+    showFilterActive(filterNew);
+    currentFilter = filterNew;
+    deletePictures();
+    picturesArrayCopy = picturesArray.slice();
+    renderPictures(getRandomArray(picturesArrayCopy, 10));
+  });
+
+  filterDiscussed.addEventListener('click', function () {
+    if (currentFilter) {
+      hideFilterActive(currentFilter);
+    }
+    showFilterActive(filterDiscussed);
+    currentFilter = filterDiscussed;
+    deletePictures();
+    picturesArrayCopy = picturesArray.slice();
+    picturesArrayCopy.sort(function (first, second) {
+      if (first.comments < second.comments) {
+        return 1;
+      } else if (first.comments > second.comments) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    renderPictures(picturesArrayCopy);
+  });
 })();
--- js/preview.js
+++ js/preview.js
@@ -1,15 +1,20 @@
 'use strict';
 
 (function () {
+  var COMMENTS_MAX = 5;
+
   var bigPicture = document.querySelector('.big-picture');
   var bigPictureImgBlock = bigPicture.querySelector('.big-picture__img');
   var bigPictureImg = bigPictureImgBlock.querySelector('img');
   var bigPictureLikesCount = bigPicture.querySelector('.likes-count');
   var bigPictureDescription = bigPicture.querySelector('.social__caption');
   var bigPictureCommentCount = bigPicture.querySelector('.social__comment-count');
-  var bigPictureCommentsLoader = bigPicture.querySelector('.comments-loader');
+  var bigPictureCommentsLoader = bigPicture.querySelector('.social__comments-loader');
   var bigPictureCommentsList = document.querySelector('.social__comments');
   var bigPictureCancel = document.getElementById('picture-cancel');
+  var picture = {};
+  var arrayComments = [];
+  var countIndexComments = 0;
 
   // Создает рандомный комментарий
   var createComment = function (comment) {
@@ -33,19 +38,23 @@
       bigPictureCommentsList.removeChild(bigPictureCommentsList.firstChild);
     }
 
-    for (var i = 0; i < object.comments.length; i++) {
-      var commentsItem = createComment(object.comments[i]);
+    for (var i = 0; i < object.length; i++) {
+      var commentsItem = createComment(object[i]);
       bigPictureCommentsList.appendChild(commentsItem);
     }
   };
 
-  var drawBigPicture = function (object) {
+
+  var showBigPicture = function (object) {
+    picture = object;
     bigPictureImg.src = object.url;
     bigPictureLikesCount.textContent = object.likes;
-    addCommentToList(object);
     bigPictureDescription.textContent = object.description;
-    bigPictureCommentCount.classList.add('visually-hidden');
-    bigPictureCommentsLoader.classList.add('visually-hidden');
+    loadNewComments();
+    // Сравнивает общее количество комментариев и показанных
+    if (picture.comments.length <= (countIndexComments)) {
+      bigPictureCommentsLoader.classList.add('visually-hidden');
+    }
   };
 
   // Закрывает по нажатию на esc
@@ -61,6 +70,7 @@
 
   // Закрывает картинку в полноэкранном режиме
   var closeBigPicture = function () {
+    resetComments();
     window.util.hideElement(bigPicture);
     document.removeEventListener('keydown', onBigPictuteEscPress);
   };
@@ -69,20 +79,50 @@
     closeBigPicture();
   });
 
+  // Очищает счетчик комментариев
+  var resetComments = function () {
+    arrayComments = [];
+    countIndexComments = 0;
+    bigPictureCommentsLoader.classList.remove('visually-hidden');
+  };
+
+  // Загружает новые комментарии
+  var loadNewComments = function () {
+    var partComments = picture.comments.slice(countIndexComments, countIndexComments + COMMENTS_MAX);
+    for (var i = 0; i < partComments.length; i++) {
+      arrayComments.push(partComments[i]);
+    }
+    addCommentToList(arrayComments);
+    countIndexComments += COMMENTS_MAX;
+
+    var allComments = picture.comments.length;
+    var openComments = bigPictureCommentsList.childNodes.length;
+    bigPictureCommentCount.innerHTML = openComments + ' из <span class="comments-count">' + allComments + '</span> комментариев</div>';
+  };
+
+  var commentsLoaderClickHandler = function () {
+    loadNewComments();
+    // Сравнивает общее количество комментариев и показанных
+    if (picture.comments.length <= (countIndexComments)) {
+      bigPictureCommentsLoader.classList.add('visually-hidden');
+    }
+  };
+
   // Подставляет адрес маленькой картинки в большую
-  var addThumbnailClickHandler = function (thumbnail, picture) {
+  var addThumbnailClickHandler = function (thumbnail, pic) {
     thumbnail.addEventListener('click', function () {
       openBigPicture();
-      drawBigPicture(picture);
+      showBigPicture(pic);
     });
   };
 
-  // Перебирает два массива и передает addThumbnailClickHandler
-  var showBigPicture = function (thumbnails, pictures) {
+  // Перебирает два массива и передает в addThumbnailClickHandler
+  var pickUpBigPicture = function (thumbnails, pictures) {
     for (var j = 0; j < thumbnails.length; j++) {
       addThumbnailClickHandler(thumbnails[j], pictures[j]);
     }
   };
 
-  window.showBigPicture = showBigPicture;
+  bigPictureCommentsLoader.addEventListener('click', commentsLoaderClickHandler);
+  window.pickUpBigPicture = pickUpBigPicture;
 })();
